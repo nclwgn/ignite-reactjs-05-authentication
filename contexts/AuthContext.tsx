1ce26ede110c81e2b66030bfdd271325b58da914
Nicholas W <nclwgn@gmail.com>
2022-06-04 12:19:50 -0300

    Refine authentication error handling

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import Router from "next/router";
 import { api } from "../services/api";
-import { setCookie, parseCookies } from "nookies";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
 
 type User = {
   email: string;
@@ -26,6 +26,13 @@ type AuthProviderProps = {
 
 const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+  destroyCookie(undefined, 'nextauth.token');
+  destroyCookie(undefined, 'nextauth.refreshToken');
+
+  Router.push('/');
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
@@ -34,11 +41,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
     const { 'nextauth.token': token } = parseCookies();
 
     if (token) {
-      api.get('me').then(response => {
-        const { email, permissions, roles } = response.data;
+      api.get('me')
+        .then(response => {
+          const { email, permissions, roles } = response.data;
 
-        setUser({ email, permissions, roles });
-      })
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          // Other errors than unauthorized
+          signOut();
+        })
     }
   }, []);
 
