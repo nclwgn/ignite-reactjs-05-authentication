31b1746cc28f840274d5a10c16d33fcb9f7abdd0
Nicholas W <nclwgn@gmail.com>
2022-07-05 21:34:51 -0300

    Validate user permissions on server side

--- hooks/useCan.ts
+++ hooks/useCan.ts
@@ -1,4 +1,5 @@
 import { useAuthContext } from "../contexts/AuthContext";
+import { validateUserPermissions } from "../utils/validateUserPermissions";
 
 type UseCanParams = {
   permissions?: string[];
@@ -11,23 +12,11 @@ export function useCan({ permissions, roles }: UseCanParams) {
   if (!isAuthenticated)
     return false;
 
-  if (permissions?.length > 0) {
-    const hasAllPermissions = permissions.every(permission => {
-      return user.permissions.includes(permission);
-    });
+  const userHasValidPermissions = validateUserPermissions({
+    user,
+    permissions,
+    roles
+  });
 
-    if (!hasAllPermissions)
-      return false;
-  }
-
-  if (roles?.length > 0) {
-    const hasAllRoles = roles.every(role => {
-      return user.roles.includes(role);
-    });
-
-    if (!hasAllRoles)
-      return false;
-  }
-
-  return true;
+  return userHasValidPermissions;
 }
