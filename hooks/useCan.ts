f74e831c21782fbf5806aa75b3d28ec904cd396c
Nicholas W <nclwgn@gmail.com>
2022-07-05 21:16:45 -0300

    Add useCan hook for permissions and roles

---
+++ hooks/useCan.ts
@@ -0,0 +1,33 @@
+import { useAuthContext } from "../contexts/AuthContext";
+
+type UseCanParams = {
+  permissions?: string[];
+  roles?: string[];
+}
+
+export function useCan({ permissions, roles }: UseCanParams) {
+  const { user, isAuthenticated } = useAuthContext();
+
+  if (!isAuthenticated)
+    return false;
+
+  if (permissions?.length > 0) {
+    const hasAllPermissions = permissions.every(permission => {
+      return user.permissions.includes(permission);
+    });
+
+    if (!hasAllPermissions)
+      return false;
+  }
+
+  if (roles?.length > 0) {
+    const hasAllRoles = roles.every(role => {
+      return user.roles.includes(role);
+    });
+
+    if (!hasAllRoles)
+      return false;
+  }
+
+  return true;
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,21 @@
 import { useAuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useAuthContext();
 
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  });
+
   return (
-    <h1>Dashboard; User {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      { userCanSeeMetrics && <div>Métricas</div> }
+    </>
   );
 }
 
